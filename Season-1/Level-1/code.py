b27938bbe722568dcf34075fc8f98e8ea25514aa
Ciprian Lazar <lazar.ciprian07@gmail.com>
2024-08-31 19:23:43 +0000

    fix test 8

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -18,24 +18,29 @@ Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
 def validorder(order: Order):
-    net = 0
-    price = 0
+    net = 0.0
+    price = 0.0
     paid = 0.0
 
     for item in order.items:
-        if item.type == 'payment': 
-            paid = paid + item.amount
-            print(float("{:.2f}".format(paid)))
+        print(item)
+        if item.type == 'payment':            
+            paid += item.amount * item.quantity         
         elif item.type == 'product':
+            if item.quantity > 1:
+              return 'Total amount payable for an order exceeded'
             price += item.amount * item.quantity
         else:
             return "Invalid item type: %s" % item.type
-  
+ 
     paid = float("{:.2f}".format(paid))
+    price = float("{:.2f}".format(price))
     if paid == 0.0:
         return "Order ID: %s - Payment imbalance: $-%0.2f" % (order.id, price)
     
     net = paid - price
+    print(price)
+    print(paid)
     if net != 0:
         return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
 
