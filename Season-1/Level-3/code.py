677f5000b552c93f684d44e3dc3430f2b2426b15
Ciprian Lazar <ciprian.lazar@ivfuture.uk>
2025-08-24 18:18:08 +0300

    Solution for level 3

--- Season-1/Level-3/code.py
+++ Season-1/Level-3/code.py
@@ -35,6 +35,9 @@ class TaxPayer:
         base_dir = os.path.dirname(os.path.abspath(__file__))
         prof_picture_path = os.path.normpath(os.path.join(base_dir, path))
 
+        if not prof_picture_path.startswith(base_dir):
+            return None
+
         with open(prof_picture_path, 'rb') as pic:
             picture = bytearray(pic.read())
 
@@ -47,6 +50,13 @@ class TaxPayer:
 
         if not path:
             raise Exception("Error: Tax form is required for all users")
+
+        # builds path
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        prof_picture_path = os.path.normpath(os.path.join(base_dir, path))
+
+        if not prof_picture_path.startswith(base_dir):
+            return None
 
         with open(path, 'rb') as form:
             tax_data = bytearray(form.read())
